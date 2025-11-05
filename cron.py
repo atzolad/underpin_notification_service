7f9f7f3c006cdade50db1245fdc535913c21df38
atzolad <atzolad@gmail.com>
2025-11-05 15:14:13 -0800

    Updated logging to just log to stdout since Google Cloud logging already saves this

--- cron.py
+++ cron.py
@@ -14,31 +14,33 @@ try:
 except Exception as e:
     print(f"Failed to startup Flask App or logger: {str(e)}")
 
-@app.route("/", methods=['POST', 'GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def trigger_notifications():
-    '''
+    """
     Endpoint for the cloud scheduler
-    'POST' or 'GET' method. 
+    'POST' or 'GET' method.
 
     Runs the main.py files as run_notifications
 
-    '''
+    """
     try:
         logger.info("Notification cron job triggered")
         run_notifications()
         return "Notifications completed successfully", 200
-    
+
     except Exception as e:
         logger.error(f"Error triggering notification cron job: {str(e)}")
-        return f"Error triggering notification cron job: {str(e)}", 
+        return (f"Error triggering notification cron job: {str(e)}",)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import os
 
-    #For local testing
-    if os.environ.get('FLASK_ENV') != 'production':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='127.0.0.1', port=port, debug=True)
+    # For local testing
+    if os.environ.get("FLASK_ENV") != "production":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="127.0.0.1", port=port, debug=True)
 
     else:
         pass
--- logger.py
+++ logger.py
@@ -1,37 +1,28 @@
 import logging
-from logging.handlers import RotatingFileHandler
+import sys
 
-def setup_logging(name="main_app_log", log_file="main_app_log", max_bytes=5000000, backupcount=2 ):
-    '''
-    Setup logging with automatic file rotation.
-    
+
+def setup_logging(name="main_app_log"):
+    """
+    Setup logging with output directly to the console which is recorded by Google Cloud Logging
+
     Args:
     name: Name of the logger (use __name__ from calling module)
-    log_file: Name of the file to save logs to. Defaults to main_app_log
-    max_bytes: Max filesize of the logfile before rotating to a new file. Default to 5MB
-    backup_count: Number of backupfiles to keep
-    
-    '''
+
+    """
     logger = logging.getLogger(name)
 
-    #Only add a logging handler if one doesn't already exist.
+    # Only add a logging handler if one doesn't already exist.
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
 
-        #Configure the logging handler
-        handler = RotatingFileHandler(
-            log_file,
-            max_bytes,
-            backupcount
+        # Configure the logging format
+        formatter = logging.Formatter(
+            "%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%SZ"
         )
 
-        #Configure the logging format
-        formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%SZ")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-        #Add console logging
-        console_handler = logging.StreamHandler()
+        # Add console logging
+        console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
