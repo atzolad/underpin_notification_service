6aed459a7ee5a431f5008a9250775e408cfcce3c
atzolad <atzolad@gmail.com>
2026-02-24 14:54:24 -0800

    Updated top of notification function to only log the customerless product email as the main notification email

--- utils/notifications.py
+++ utils/notifications.py
@@ -315,9 +315,14 @@ def send_notifications(messages: list):
             for message in messages:
                 try:
                     server.send_message(message.message)
-                    logger.info(
-                        f"Email sent successfully to  {message.customer.name} at {message.customer.email} and {notification_address} "
-                    )
+                    if message.customer.email == notification_address:
+                        logger.info(
+                            f"Email sent successfully to {message.customer.name} at {notification_address}"
+                        )
+                    else:
+                        logger.info(
+                            f"Email sent successfully to  {message.customer.name} at {message.customer.email} and {notification_address} "
+                        )
                     successfully_sent += 1
                     notification_status = "sent"
                     notification_rows = add_notification_row(
